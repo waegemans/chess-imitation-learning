bece54ee1dc191d84bf6052f316e7c04d00f88d3
waegemans <45630999+waegemans@users.noreply.github.com>
2019-10-18 11:47:42 +0200

    added smaller network

--- src/models.py
+++ src/models.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+import numpy as np
+
 class ssf_asf_1024_1024(nn.Module):
   def __init__(self):
     super(ssf_asf_1024_1024,self).__init__()
@@ -14,5 +16,24 @@ class ssf_asf_1024_1024(nn.Module):
 
   def forward(self, state):
     return self.net(state)
+    
+    
+class ssf_asf_512_512(nn.Module):
+  def __init__(self):
+    super(ssf_asf_512_512,self).__init__()
+    self.net = nn.Sequential(
+		  nn.Linear(773,512),
+		  nn.ReLU(),
+		  nn.Linear(512,512),
+		  nn.ReLU(),
+		  nn.Linear(512,4096)
+		)
+
+  def forward(self, state):
+    return self.net(state)
+    
+def number_train_params(model):
+  train_params = filter(lambda p: p.requires_grad, model.parameters())
+  return sum(np.prod(ti.shape) for ti in train_params)
 
 
--- src/vis.py
+++ src/vis.py
@@ -11,22 +11,18 @@ def smooth_exp(x):
       y[i] = 0.9*y[i-1]+0.1*xi
   return y
 
-data = pd.read_csv('1000_nll_4move.csv')
+data = pd.read_csv('1kpos_80_20split_simple_64bs.csv')
 
 fig,ax = plt.subplots(2)
-plt.title("Stockfish opening 4 moves - batchsize=1")
+plt.title("Stockfish 1k positions")
 
-ax[0].plot(data['mse_loss'],label='NLL', alpha=0.2)
-ax[0].plot(np.convolve(data['mse_loss'], np.ones(9)/9, 'same'), label='NLL smooth')
+ax[0].plot(data['train_cross_entropy_loss'],label='Train Cross entropy')
+ax[0].plot(data['val_cross_entropy_loss'],label='Train Cross entropy')
 
-ax[1].plot(smooth_exp(data.stockfish == data.predicted), label='train_accuracy')
-'''
-ax[1].plot(np.cumsum(data.stockfish == 'e2e4')/(data.sample_nr//2+1), label='e2e4_stockfish')
-ax[1].plot(np.cumsum(data.stockfish == 'c2c4')/(data.sample_nr//2+1), label='c2c4_stockfish')
-ax[1].plot(np.cumsum(data.stockfish == 'd2d4')/(data.sample_nr//2+1), label='d2d4_stockfish')
-ax[1].plot(np.cumsum(data.stockfish == 'e2e3')/(data.sample_nr//2+1), label='e2e3_stockfish')
-ax[1].plot(np.cumsum(data.stockfish == 'g1f3')/(data.sample_nr//2+1), label='g1f3_stockfish')
-'''
+
+ax[1].plot(data['train_acc'],label='Training acc')
+ax[1].plot(data['val_acc'],label='Validation acc')
+
 ax[1].legend()
 ax[0].legend()
 plt.show()
