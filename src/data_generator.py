3bc0d3d2067735ebc11bcaed42d72b5e63db47ec
waegemans <45630999+waegemans@users.noreply.github.com>
2020-01-15 16:56:18 +0100

    cp datagen

--- src/data_generator.py
+++ src/data_generator.py
@@ -1,39 +1,49 @@
 import chess
 import chess.engine
-import csv
 import numpy as np
+import time
+import os
+import csv
 
-d = {}
-with open("data/moves.csv", "r") as csv_file:
-    r = csv.reader(csv_file)
-    for fen, move in r:
-        d[fen] = move
-
-w = csv.writer(open("data/moves.csv", "a"))
-
-engine = chess.engine.SimpleEngine.popen_uci("../engine/stockfish_10_x64")
-engine.configure(options={'Threads': 8})
+n_threads = 8
+depth = 18
+gamma = 1
+mate_score=10000
 
 board = chess.Board()
+engine = chess.engine.SimpleEngine.popen_uci("../engine/stockfish_10_x64_modern")
+engine.configure(options={'Threads': n_threads})
 
+game_id = 0
+
+explored = {}
+path = "data/depth%d_gamma%d/"%(depth,gamma)
+os.makedirs(path,exist_ok=True)
+
+with open(path+"moves.csv","r") as f:
+    r = csv.reader(f)
+    for fen,_ in r:
+        explored[fen] = True
+
+writer = csv.writer(open(path+"moves.csv","a"))
 
 while True:
-  if (board.is_game_over()):
-      # reset board if game is over
-      board.reset()
-  fen_without_count = ' '.join(board.fen().split(' ')[:-2])
+    fen_without_count = ' '.join(board.fen().split()[:-2])
+    if board.is_game_over():
+        board.reset()
+        game_id += 1
+    if not board.turn:
+        board = board.mirror()
 
-  move = None
-
-  if fen_without_count not in d.keys():
-        result = engine.play(board, chess.engine.Limit(depth=10))
-        d[fen_without_count] = result.move.uci()
-        w.writerow([fen_without_count, result.move.uci()])
-        move = result.move
-  if np.random.uniform() < 0.1:
-      board.push(np.random.choice(list(board.legal_moves)))
-  else:
-      board.push_uci(d[fen_without_count])
+    if fen_without_count in explored.keys():
+        board.push(np.random.choice(list(board.legal_moves)))
+        continue
+    
+    info = engine.analyse(board,chess.engine.Limit(depth=depth),multipv=10,game=game_id)
+    d = {}
+    for i in info:
+        d[i['pv'][0].uci()] = i['score'].pov(1).score(mate_score=mate_score)
+    explored[fen_without_count] = True
+    writer.writerow([fen_without_count,d])
 
 engine.quit()
-
