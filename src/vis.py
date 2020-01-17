71a2cba99a55b4471e8f15a99e8887cadea75bc9
waegemans <45630999+waegemans@users.noreply.github.com>
2020-01-17 14:23:01 +0100

    all data

--- src/vis.py
+++ src/vis.py
@@ -56,7 +56,7 @@ for x in range(0,data['batch_count'].values[-1],batches_per_epoch):
   ax[0].axvline(x=x, ymin=0.0, ymax=1.0, color='r', alpha=0.1)
   ax[1].axvline(x=x, ymin=0.0, ymax=1.0, color='r', alpha=0.1)
   ax[2].axvline(x=x, ymin=0.0, ymax=1.0, color='r', alpha=0.1)
-ax[0].set_ylim(bottom=0,top=1e4)
+
 ax[1].legend()
 ax[0].legend()
 plt.show()
