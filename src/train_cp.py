4f51bf782874f632040f13352bb14372c92369df
waegemans <45630999+waegemans@users.noreply.github.com>
2020-01-23 15:56:39 +0100

    cross entropy pretrained

--- src/train_cp.py
+++ src/train_cp.py
@@ -53,7 +53,7 @@ def loss_fcn(predicted, target, mask):
   mse = nn.functional.mse_loss(torch.flatten(predicted*mask),torch.flatten(target*mask),reduction='sum') / mask.sum()
   hinge = (nn.functional.relu((predicted-target)*(1-mask))**2).sum() / (1-mask).sum()
   cross_entropy = nn.functional.cross_entropy(predicted, target.argmax(dim=1),reduction='mean')
-  return mse + hinge
+  return cross_entropy
 
 total_batch_count = 0
 running_train_loss = None
