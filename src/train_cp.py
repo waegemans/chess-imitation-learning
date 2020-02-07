056b61232fe63011d0eca6cf37c7910b89b00950
waegemans <45630999+waegemans@users.noreply.github.com>
2020-02-07 16:17:18 +0100

    mse+hinge

--- src/train_cp.py
+++ src/train_cp.py
@@ -64,13 +64,13 @@ def multi_cross_entropy(predicted, target, mask, topn=5):
 
 
 def loss_fcn(predicted, target, mask):
-  #mse = nn.functional.mse_loss(torch.flatten(predicted*mask),torch.flatten(target*mask),reduction='sum') / mask.sum()
-  #hinge = (nn.functional.relu((predicted-target)*(1-mask))**2).sum() / (1-mask).sum()
-  cross_entropy = nn.functional.cross_entropy(predicted, target.argmax(dim=1),reduction='mean')
+  mse = nn.functional.mse_loss(torch.flatten(predicted*mask),torch.flatten(target*mask),reduction='sum') / mask.sum()
+  hinge = (nn.functional.relu((predicted-target)*(1-mask))**2).sum() / (1-mask).sum()
+  #cross_entropy = nn.functional.cross_entropy(predicted, target.argmax(dim=1),reduction='mean')
   #avg_cp_loss = -(nn.functional.softmax(predicted)*target).view(len(target),-1).sum(1).mean()
   #return avg_cp_loss
   #m_cross_entropy = multi_cross_entropy(predicted, target, mask)
-  return cross_entropy 
+  return mse + hinge 
 
 total_batch_count = 0
 running_train_loss = None
