262aa8f5ee2a563699b6b2361e5ab510749f5501
waegemans <45630999+waegemans@users.noreply.github.com>
2020-02-21 10:28:32 +0100

    only uniq positions

--- src/data/train_val_split_statevalue.sh
+++ src/data/train_val_split_statevalue.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-grep -e ' w ' -v moves_statevalue.csv | shuf > moves_statevalue_shuf.csv
+grep -e ' w ' -v moves_statevalue.csv | sort -u -t, -k1,1 | shuf > moves_statevalue_shuf.csv
 val=$(bc <<< $(cat moves_statevalue_shuf.csv | wc -l)*0.1/1)
 head -n -$val moves_statevalue_shuf.csv > moves_statevalue_train.csv
 tail -n -$val moves_statevalue_shuf.csv > moves_statevalue_val.csv
