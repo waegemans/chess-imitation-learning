52aa086c498e8b4ea45edafd09c9ffe2148d6bc9
waegemans <45630999+waegemans@users.noreply.github.com>
2020-03-02 11:26:12 +0100

    bug

--- src/train_value.py
+++ src/train_value.py
@@ -55,7 +55,7 @@ total_batch_count = 0
 running_train_loss = None
 
 def sum_grads(model):
-  train_params = filter(lambda p: p.requires_grad, model.parameters())
+  train_params = filter(lambda p: p.requires_grad and p.grad is not None, model.parameters())
   return sum(ti.grad.detach().cpu().abs().sum().numpy() for ti in train_params)
 
 def validate_batch():
