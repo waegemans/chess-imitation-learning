6a54b5db8ee3b489a2601361875a6d0ee2eb6de3
waegemans <45630999+waegemans@users.noreply.github.com>
2020-03-06 14:40:17 +0100

    l1 siam

--- src/train_siam.py
+++ src/train_siam.py
@@ -30,7 +30,7 @@ model = models.cnn_siam().to(device)
 optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-3, momentum=.9)
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.67, patience=0, verbose=True, threshold=1e-2)
 
-trainset,valset = ChessMoveDataset_statevalue_it(discretize=True),ChessMoveDataset_statevalue_it(discretize=True,mode='val')
+trainset,valset = ChessMoveDataset_statevalue_it(),ChessMoveDataset_statevalue_it(mode='val')
 
 train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=False, num_workers=8, drop_last=True)
 val_loader = torch.utils.data.DataLoader(valset, batch_size=batch_size, shuffle=False)
@@ -48,7 +48,7 @@ def multi_cross_entropy(predicted, target, mask, topn=5):
 
 
 def loss_fcn(predicted, target):
-  return nn.functional.l1_loss(predicted,((target-util.shift(target))>0).float())
+  return nn.functional.l1_loss(predicted,(target-util.shift(target)).float())
 
 def acc_fnc(predicted,target):
     return ((predicted>0) == ((target-util.shift(target))>0)).cpu().numpy().mean()
