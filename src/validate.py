9816d132e9f0ad206baf75e991dfa67a12131be2
waegemans <45630999+waegemans@users.noreply.github.com>
2020-04-03 11:39:02 +0200

    monte carlo validation

--- src/validate.py
+++ src/validate.py
@@ -1,31 +1,39 @@
 import models
 import dataset
 
+import numpy as np
 import progressbar
 import torch
 import glob
 ## 0ab90067a02d8eb69c5aa4756eeed062d4872c5a
-ds = dataset.ChessMoveDataset_pre_it_pov_cnn('val')
-dl = torch.utils.data.DataLoader(ds,batch_size=1024,shuffle=False, num_workers=8)
+ds = dataset.ChessMoveDataset_cp_it(mode='val')
+dl = torch.utils.data.DataLoader(ds,batch_size=1024,shuffle=False, num_workers=8, drop_last=True)
 
 
 device = ('cuda:0' if torch.cuda.is_available() and torch.cuda.device_count() > 0 else 'cpu')
 
 with torch.no_grad():
-    for model_file in glob.glob("output/0ab90067a02d8eb69c5aa4756eeed062d4872c5a/*.nn"):
+    for model_file in glob.glob("output/0aff195fdf8987061724c6eebb39bb04099a577e/model_ep66.nn"):
         print(model_file)
         model = torch.load(model_file,map_location=device)
-        model.eval()
+        model.train()
         
-        val_acc = 0
-        n_samples = 0
+        accs = []
+        conf_cor = []
+        conf_wro = []
+        conf = []
 
-        for x,y in progressbar.progressbar(dl):
-            x,y = x.to(device),y.to(device)
+        for x,c,m,l in progressbar.progressbar(dl):
+            x,c,m,l = x.to(device),c.to(device),m.to(device),l.to(device)
 
-            predicted = model(x)
+            predicted = torch.stack([model(x).masked_fill(l==0,-float('inf')).argmax(dim=1) for _ in range(10)],dim=1)
+            top_label,_ = predicted.mode()
+            correct = top_label == c.argmax(dim=1)
+            acc = correct.float().mean().item()
+            confidence = (predicted.T == top_label).float().mean(dim=0)
+            accs.append(acc)
+            conf_cor.append(confidence[correct].mean().item())
+            conf_wro.append(confidence[~correct].mean().item())
+            conf.append(confidence.mean().item())
 
-            n_samples += len(x)
-            val_acc += (predicted.detach().argmax(dim=1) == y).cpu().numpy().sum()
-
-        print(model_file, val_acc/n_samples)
+        print(np.mean(accs),np.mean(conf_cor),np.mean(conf_wro),np.mean(conf))
